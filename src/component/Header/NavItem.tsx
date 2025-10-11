267aeb90109011f5cf70eee2a3643e5b7440b4b6
Ha Quanq <quangha0120010@gmail.com>
2025-10-11 21:05:40 +0700

    Fix nav link hover effect (spacing)

--- src/component/Header/NavItem.tsx
+++ src/component/Header/NavItem.tsx
@@ -8,7 +8,7 @@ export const NavItem = (props: NavItemProps) => {
       <a class="peer focus-v block transition-[color] hover:text-black" href="">
         {props.label}
       </a>
-      <div class="absolute top-[calc(100%+2.25rem)] right-0 left-0 hidden h-1 bg-orange-500 opacity-0 transition-opacity peer-hover:opacity-100 md:top-[calc(100%+2.875rem)] md:block"></div>
+      <div class="absolute right-0 left-0 hidden h-1 bg-orange-500 opacity-0 transition-opacity peer-hover:opacity-100 md:top-[calc(100%+2.25rem)] md:block lg:top-[calc(100%+2.875rem)]"></div>
     </li>
   );
 };
